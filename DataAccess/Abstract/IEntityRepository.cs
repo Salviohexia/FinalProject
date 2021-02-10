8781bb52fdcac50e1a4e7747b883b02d96328e9a
Yigit Coskun <ygtcskn@outlook.com>
2021-02-10 20:24:05 +0300

    Entity framework

---
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -0,0 +1,18 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Abstract
+{
+    public interface IEntityRepository<T> where T:class,IEntity,new()                                          //generic constraint-jenerik kısıt   // class : referans tip olabilir
+    {                                                                                                          //new() : new'lenebilir olmalı kısıtı
+        List<T> GetAll(Expression<Func<T,bool>> filter=null);
+        T get(Expression<Func<T, bool>> filter);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+
+    }
+}
