159d8a5856f0d55d167fd892896eadf4159755cd
Yigit Coskun <ygtcskn@outlook.com>
2021-02-13 20:49:29 +0300

    13 şubat

--- Core/DataAccess/IEntityRepository.cs
+++ Core/DataAccess/IEntityRepository.cs
@@ -1,10 +1,10 @@
-﻿using Entities.Abstract;
+﻿using Core.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq.Expressions;
 using System.Text;
 
-namespace DataAccess.Abstract
+namespace Core.DataAccess
 {
     public interface IEntityRepository<T> where T:class,IEntity,new()                                          //generic constraint-jenerik kısıt   // class : referans tip olabilir
     {                                                                                                          //new() : new'lenebilir olmalı kısıtı
